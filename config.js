dd1fddfd03e2a2446e40b23790e1687608c37b27
khanhhon <92222607+khanhhon@users.noreply.github.com>
2021-10-09 22:58:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introTitle: 'Hé lù Bà Chã iuuuuu',
+    introDesc: `Hum nai là Kỷ nịm anh với em 1 lăm iu nhao á, iu baby của anh nhiều nhắm `,
+    btnIntro: 'Dạaaaaaa',
+    title: 'HAPPY ANIVERSARY 1YEAR',
+    desc: 'Em có iu anh khummmmmm ',
+    btnYes: 'Dạ cóaaa',
+    btnNo: 'Đéo',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Yêu em nhất lun thế giới',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
